b4692be18d78cdcef064522f11f654ec70a63cd7
arye <mafshar85@hotmail.com>
2023-05-11 09:00:47 +0000

    Add server test file

---
+++ app/server.test.js
@@ -0,0 +1,16 @@
+const path = require( 'path');
+const fs = require(' fs' )
+
+test('main index.html file exists', () => {
+  const filePath = path.join(--dirname, "index.html") 
+  expect(fs.existsSync(filePath)).toBeTruthy();
+});
+test('Dockerfile exists', () => {
+  const filePath = path.join(--dirname, ".." ,"Dockerfile")
+  expect(fs.existsSync(filePath)).toBeTruthy();
+});
+
+test('.gitignore file exists', () =>{
+  const filePath = path.join(__dirname, ".."  , ".gitignore")
+  expect(fs.existsSync(filePath)).toBeTruthy();
+});
